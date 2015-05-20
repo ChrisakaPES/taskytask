7c6c3430346b9d5fd1dbd27185b9ce93abfe66a9
Chris Quintanilla <caq977@gmail.com>
2015-05-20 11:43:17 -0600

    dashboard.jsp and Model classes

---
+++ src/main/java/edu/neumont/dal/SlateHandler.java
@@ -0,0 +1,18 @@
+package edu.neumont.dal;
+
+import java.time.LocalDateTime; 
+
+import edu.neumont.models.Slate;
+public class SlateHandler 
+{ 
+	public void createSlate(String name, String description, LocalDateTime dueDate) 
+	{ } 
+	public Slate retrieveSlate() 
+	{ 
+		Slate s; return null; 
+	} 
+	public void updateSlate() 
+	{ } 
+	void deleteSlate() 
+	{ } 
+}
---
+++ src/main/java/edu/neumont/models/Slate.java
@@ -0,0 +1,38 @@
+package edu.neumont.models;
+import java.time.LocalDateTime; 
+public class Slate 
+{ 
+	private String name; 
+	private String description; 
+	private LocalDateTime dueDate; 
+	public Slate(String name, String description, LocalDateTime dueDate) 
+	{ 
+		this.name = name; 
+		this.description = description; 
+		this.dueDate = dueDate; 
+	} 
+	public String getName() 
+	{ 
+		return name; 
+	} 
+	public void setName(String name) 
+	{ 
+		this.name = name;
+	} 
+	public String getDescription() 
+	{ 
+		return description; 
+	} 
+	public void setDescription(String description) 
+	{ 
+		this.description = description; 
+	} 
+	public LocalDateTime getDueDate() 
+	{ 
+		return dueDate;
+	} 
+	public void setDueDate(LocalDateTime dueDate) 
+	{ 
+		this.dueDate = dueDate; 
+	} 
+}
